d405f7fab1b0a84ba9377c600b58e765c0a46aa9
QOZU <64510789+QOZU@users.noreply.github.com>
2025-08-06 04:57:33 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/searchfilter/main.go
@@ -0,0 +1,23 @@
+// cmd/searchfilter/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"searchfilter/internal/searchfilter"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := searchfilter.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
